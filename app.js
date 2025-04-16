bf4205952ac0064ac2983ed68a2ea56aff0c7b0f
danielemurgioni <dashomew@gmail.com>
2025-04-16 17:44:36 +0200

    [UPD] Server setup and test with postman

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+//importo express js
+const express = require("express");
+//assegno una funzione a una variabile
+const app = express();
+//inizializzo il numero della porta
+const port = 3000;
+
+//creo la rotta madre del blog
+app.get("/", (req,res) =>{
+    res.send("Server del mio blog")
+})
+
+//il server rimane in ascolto
+app.listen(port, () =>{
+    console.log(`Il Server è in ascolto nella porta ${port}`)
+})
